291c026130f9a09a07effc76b66aacae45d98026
spider-yamet <needmefordev@gmail.com>
2026-02-08 13:47:40 -0800

    Modification on 03820 for error code validation

--- tests/queries/0_stateless/03820_kusto_functions_empty_argument_validation.sql
+++ tests/queries/0_stateless/03820_kusto_functions_empty_argument_validation.sql
@@ -2,4 +2,4 @@
 SET allow_experimental_kusto_dialect = 1;
 SET dialect = 'kusto';
 
-print bin(, 1.5); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+print bin(, 1.5); -- { error NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
