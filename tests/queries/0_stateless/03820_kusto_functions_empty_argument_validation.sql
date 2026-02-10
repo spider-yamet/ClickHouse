3eec1a7077ded40d5f8182262da40909acf8b059
spider-yamet <needmefordev@gmail.com>
2026-02-09 20:04:46 -0800

    Remove spaces in 03820 test file

--- tests/queries/0_stateless/03820_kusto_functions_empty_argument_validation.sql
+++ tests/queries/0_stateless/03820_kusto_functions_empty_argument_validation.sql
@@ -5,5 +5,3 @@ SET dialect = 'kusto';
 print bin(, 1.5); -- { error NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 
 print extract("User: ([^,]+)",, "User: James, Email: James@example.com, Age: 29"), -- { clientError SYNTAX_ERROR }
-
-
